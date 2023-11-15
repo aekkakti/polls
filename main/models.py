6e5d62ae4dda5166c00afdf19c493246263f54a6
aekkakti <music.tomsk1@gmail.com>
2023-11-15 15:34:35 +0700

    change checkboxes

--- main/models.py
+++ main/models.py
@@ -38,10 +38,9 @@ class Poll(models.Model):
         return self.question
 
 class Choice(models.Model):
-    poll = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    question = models.ForeignKey(Poll, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
     def __str__(self):
         return self.choice_text
-
